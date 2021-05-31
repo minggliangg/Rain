7023314d9bef7a280f821748d9cff475b509c3d0
Ming Liang <khongmingliang@icloud.com>
2021-05-31 15:42:16 +0800

    Initial Commit

---
+++ Rain/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Rain
+//
+//  Created by Ming Liang Khong on 31/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        label.text = "Hello World!"
+    }
+
+
+}
+
